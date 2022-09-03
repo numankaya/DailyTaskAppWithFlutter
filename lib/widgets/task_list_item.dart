cf344aab882abfa105cc00cf62981f03d9c7b9a4
Numan Kaya <savletnumankaya@gmail.com>
2022-09-03 19:45:03 +0300

    first_commit

---
+++ lib/widgets/task_list_item.dart
@@ -0,0 +1,89 @@
+import 'package:dailytodo/data/local_storage.dart';
+import 'package:dailytodo/main.dart';
+import 'package:dailytodo/models/task_model.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TaskItem extends StatefulWidget {
+  final Task task;
+  TaskItem({Key? key, required this.task}) : super(key: key);
+
+  @override
+  State<TaskItem> createState() => _TaskItemState();
+}
+
+class _TaskItemState extends State<TaskItem> {
+  final TextEditingController _taskNameController = TextEditingController();
+  late LocalStorage _localStorage;
+
+  @override
+  void initState() {
+    super.initState();
+    _localStorage = locator<LocalStorage>();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _taskNameController.text = widget.task.name;
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(.2),
+            blurRadius: 10,
+          ),
+        ],
+      ),
+      child: ListTile(
+        autofocus: true,
+        leading: GestureDetector(
+          onTap: () {
+            widget.task.isCompleted = !widget.task.isCompleted;
+            _localStorage.updateTask(task: widget.task);
+            setState(() {});
+          },
+          child: Container(
+            child: Icon(
+              Icons.check,
+              color: Colors.white,
+            ),
+            decoration: BoxDecoration(
+              color: widget.task.isCompleted ? Colors.green : Colors.white,
+              border: Border.all(color: Colors.grey, width: 0.8),
+              shape: BoxShape.circle,
+            ),
+          ),
+        ),
+        title: widget.task.isCompleted
+            ? Text(
+                widget.task.name,
+                style: TextStyle(
+                    decoration: TextDecoration.lineThrough, color: Colors.grey),
+              )
+            : TextField(
+                controller: _taskNameController,
+                minLines: 1,
+                maxLines: null,
+                textInputAction: TextInputAction.done,
+                decoration: InputDecoration(border: InputBorder.none),
+                onSubmitted: (yeniDeger) {
+                  if (yeniDeger.length > 3) {
+                    widget.task.name = yeniDeger;
+                    _localStorage.updateTask(task: widget.task);
+                  }
+                },
+              ),
+        trailing: Text(
+          DateFormat("hh:mm a").format(widget.task.createdAt),
+          style: TextStyle(
+            fontSize: 14,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+}
